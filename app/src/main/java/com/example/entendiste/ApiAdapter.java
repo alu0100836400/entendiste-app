c1803df1064158072c5db510974b12ed9e63057e
alu0100836400 <alu0100836400@ull.edu.es>
2021-09-01 21:09:04 +0100

    sync

--- app/src/main/java/com/example/entendiste/ApiAdapter.java
+++ app/src/main/java/com/example/entendiste/ApiAdapter.java
@@ -21,7 +21,8 @@ public class ApiAdapter {
         httpClient.addInterceptor(logging);
 
         //String baseUrl = "http://entendiste.local/api/";
-        String baseUrl = "http://192.168.1.55/api/";
+        //String baseUrl = "http://192.168.1.55/api/";
+        String baseUrl = "http://192.168.1.105/api/";
 
         if (API_SERVICE == null) {
             Retrofit retrofit = new Retrofit.Builder()
